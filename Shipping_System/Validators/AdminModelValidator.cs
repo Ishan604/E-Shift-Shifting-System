30b71850f1b6ecbdb74afe80912d2ac3703c9a6e
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-07-20 16:00:01 +0530

    Add files via upload

---
+++ Shipping_System/Validators/AdminModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Shipping_System.Models;
+
+namespace Shipping_System.Validators
+{
+    public class AdminModelValidator : AbstractValidator<AdminModel>
+    {
+        public AdminModelValidator()
+        {
+            RuleFor(admin => admin.Firstname)
+                .NotEmpty().WithMessage("First name is required.")
+                .Length(2, 50).WithMessage("First name must be between 2 and 50 characters.");
+            RuleFor(admin => admin.Lastname)
+                .NotEmpty().WithMessage("Last name is required.")
+                .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters.");
+            RuleFor(admin => admin.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+            RuleFor(admin => admin.Phonenumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Matches(@"^\d{10}$").WithMessage("Phone number must be exactly 10 digits.");
+            RuleFor(admin => admin.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(15).WithMessage("Password must be at least 15 characters long.");
+        }
+    }
+}
---
+++ Shipping_System/Validators/JobsModelValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Shipping_System.Models;
+
+namespace Shipping_System.Validators
+{
+    public class JobsModelValidator : AbstractValidator<JobsModel>
+    {
+        public JobsModelValidator()
+        {
+            RuleFor(job => job.fullName)
+                .NotEmpty().WithMessage("Full name is required.")
+                .Length(2, 100).WithMessage("Full name must be between 2 and 100 characters.");
+            RuleFor(job => job.startLocation)
+                .NotEmpty().WithMessage("Start location is required.")
+                .Length(2, 200).WithMessage("Start location must be between 2 and 200 characters.");
+            RuleFor(job => job.destination)
+                .NotEmpty().WithMessage("Destination is required.")
+                .Length(2, 200).WithMessage("Destination must be between 2 and 200 characters.");
+            RuleFor(job => job.email)
+                .EmailAddress().WithMessage("Invalid email format.");
+            RuleFor(job => job.phoneNumber)
+                .Matches(@"^\d{10}$").WithMessage("Phone number must be exactly 10 digits.");
+            RuleFor(job => job.itemDescription)
+                .NotEmpty().WithMessage("Item description is required.")
+                .Length(5, 500).WithMessage("Item description must be between 5 and 500 characters.");
+        }
+    }
+}
