4a680036e1468395a59e9c4d9ab6edc0326c9744
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-07-28 17:35:08 +0530

    Add files via upload

--- Shipping_System/Repository/Service/AssignedJobRepository.cs
+++ Shipping_System/Repository/Service/AssignedJobRepository.cs
@@ -30,5 +30,36 @@ namespace Shipping_System.Repository.Service
                 command.ExecuteNonQuery();
             }
         }
+
+        public List<AssignedJobModel> GetAssignedJobDetailsByJobId(int jobId)
+        {
+            List<AssignedJobModel> assignedJobs = new List<AssignedJobModel>();
+
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                var query = "SELECT * FROM AssignedJobs WHERE job_id = @jobid";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@jobid", jobId);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    AssignedJobModel assignedJob = new AssignedJobModel
+                    {
+                        Id = Convert.ToInt32(reader["assigned_id"]),
+                        JobId = Convert.ToInt32(reader["job_id"]),
+                        LoadId = Convert.ToInt32(reader["load_id"]),
+                        ContainerId = Convert.ToInt32(reader["container_id"]),
+                        UnitId = Convert.ToInt32(reader["unit_id"]),
+                        LoadItems = reader["load_description"].ToString(),
+                        Weight = reader["load_weight"].ToString(),
+                        AssignedDate = Convert.ToDateTime(reader["assigned_date"])
+
+                    };
+                    assignedJobs.Add(assignedJob);
+                }
+            }
+            return assignedJobs;
+        }
     }
 }
